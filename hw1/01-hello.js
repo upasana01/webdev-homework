9baa0a73d9077c18ded8ea2e33831c97ba4c53d9
upchaudhari <upasana@pdx.edu>
2023-04-17 15:41:32 -0700

    Home work 1 completed

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,11 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(` My name is Upasana Chaudhari. I am a graduate student pursuing MS in
+  Computer Science. Previously, I was working as an Analyst in Data
+  Science at Novartis, India for 3 and half years. I also hold a masters
+  degree in computer science and engineering specialization in big data
+  analytics and a bachelor's degree in Information technology from
+  India.`);
 }
